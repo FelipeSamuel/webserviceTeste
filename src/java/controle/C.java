3650f4234aa3fe4f760b16727844a5200be31b44
max <max@max-System-Product-Name>
2016-05-02 18:18:56 -0300

    primeiro commit

---
+++ src/java/controle/C.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controle;
+
+//import com.mysql.jdbc.Connection;
+import java.sql.Connection;
+
+/**
+ *
+ * @author max
+ */
+public class C {
+    private static final String URL = "jdbc:mysql://localhost/dbTeste";
+    private static final String DRIVER = "com.mysql.jdbc.driver";
+    private static final String USER = "root";
+    private static final String PSW = " DL310184";
+    
+    private static Connection conn;
+    
+}
